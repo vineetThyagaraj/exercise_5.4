3bbfe87402e9c8c93303d3be50f9883e767ccdf4
vineet-thyagaraj <vineetgowda.thyagaraj@tekeinstein.com>
2025-04-11 18:49:31 -0400

    created kilometer variable to store coverter miles value.

--- src/milesToKilometer.java
+++ src/milesToKilometer.java
@@ -3,7 +3,8 @@ public class milesToKilometer {
     public static void main(String[] args) throws Exception {
         System.out.printf("%s %10s %s\n", "Miles", "", "Kilometers");
         for(int i=1; i <= 10; i++){
-            System.out.printf("%d %-17s %.3f\n", i, "", (i*1.609));
+            double kilometer = i*1.609;
+            System.out.printf("%d %-17s %.3f\n", i, "", kilometer);
         }
     
     }
